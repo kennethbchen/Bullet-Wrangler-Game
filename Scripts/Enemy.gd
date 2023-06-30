19dc547ffed1084de6d46149fc22a3c0e6a4c87a
kennethbchen <kennethbchen@gmail.com>
2023-06-30 14:05:38 -0400

    Gave enemy healthSystem

---
+++ Scripts/Enemy.gd
@@ -0,0 +1,22 @@
+extends Area2D
+
+@export var target_node: Node2D
+
+@onready var health_system = $HealthSystem
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _on_area_entered(area : Area2D):
+	pass
+
+func take_damage(damage: int):
+	health_system.change_health(-abs(damage))
+
+func _on_health_zeroed():
+	print("enemy ded")
