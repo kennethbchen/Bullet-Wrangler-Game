c5d07a1fa291432d92a9cb3e0987853225bac339
kennethbchen <kennethbchen@gmail.com>
2023-07-01 17:58:53 -0400

    Made basic enemy movement system

--- Scripts/Enemy.gd
+++ Scripts/Enemy.gd
@@ -2,18 +2,34 @@ extends CharacterBody2D
 
 class_name Enemy
 
+@export var patrol_point_parent: Node
+
 @export var target_node: Node2D
+
+@export var speed: float = 30
+
+@export var patrol_max_radius: float = 20
 
 @onready var health_system = $HealthSystem
 
+var rand: RandomNumberGenerator
+
+var current_patrol_target: Node2D
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	rand = RandomNumberGenerator.new()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
+func _physics_process(delta):
+	
+	if current_patrol_target == null or global_position.distance_to(current_patrol_target.global_position) < patrol_max_radius:
+		current_patrol_target = patrol_point_parent.get_children()[rand.randi_range(0, patrol_point_parent.get_children().size() - 1)]
+	
+	velocity += (current_patrol_target.position - global_position).normalized() * speed * delta
+	
+	move_and_slide()
+	
+	
 func take_damage(damage: int):
 	health_system.change_health(-abs(damage))
 
