bc4289c574785780ff1602159352d50801e045a6
kennethbchen <kennethbchen@gmail.com>
2023-07-01 13:02:30 -0400

    Improved projectile returning system

--- Scripts/Projectile.gd
+++ Scripts/Projectile.gd
@@ -2,8 +2,12 @@ extends Area2D
 
 class_name Projectile
 
+enum State {IDLE, TRAVELLING, RETURNING}
+
 @export var speed: float = 60
 @export var lifespan: float = 5
+
+var current_state: State = State.TRAVELLING
 
 var original_owner: Node2D
 var current_owner: Node2D
@@ -18,13 +22,43 @@ func _ready():
 	pass
 
 func _process(delta):
-	position += transform.x * speed * delta
+	
+	if current_state == State.TRAVELLING:
+		position += transform.x * speed * delta
+	elif current_state == State.RETURNING:
+		
+		position += transform.x * speed * delta
+		
+		if is_instance_valid(original_owner):
+			rotation = lerp_angle(rotation, rotation + get_angle_to(original_owner.global_position), delta * 10)
+
 
 func change_owner(new_owner: Node2D):
 	current_owner = new_owner
 
 func return_to_owner():
-	transform.x = -transform.x
+	
+	if current_state != State.TRAVELLING:
+		return
+		
+	current_state = State.IDLE
+	
+
+	var tween = get_tree().create_tween()
+	tween.set_ease(Tween.EASE_OUT)
+	tween.set_trans(Tween.TRANS_QUAD)
+	tween.tween_property(self, "position", position + transform.x * 10, 0.3)
+	tween.tween_callback(func(): 
+		
+		if is_instance_valid(original_owner):
+			rotation = rotation + get_angle_to(original_owner.global_position)
+			
+		current_state = State.RETURNING
+		speed = speed + 80
+		)
+	
+	
+	
 
 func _on_area_entered(area: Area2D):
 	
